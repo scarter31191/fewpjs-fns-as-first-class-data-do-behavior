77cff7f998a5b7f7e336c5ddeedc4c04e6fca93e
scarter31191 <scarter31191@gmaile.com>
2021-03-06 21:13:07 -0500

    commit tree 937008ceb61c6619756080236ed25b63f739df81

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+function displayMessage(msg) {
+  document.getElementById('greeting').innerText = msg; // updates DOM with appropriate message
+}
 
+function greet(e) {
+  const hour = parseInt(e, 10);
+   if( hour < 12 ) return "Good Morning"
+   if ( hour > 17 ) return "Good Evening"
+   return "Good Afternoon"
+}
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
